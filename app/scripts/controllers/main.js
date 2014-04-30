a8734069850920f7c70d1e3e572cc572a5d949ad
Haykel Ben Jemia <hbj@allmas.tn>
2014-04-30 19:26:56 +0100

    Move ‘work completed’ controller to main script file.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,4 +2,21 @@
 
 angular.module('smart2App')
   .controller('MainCtrl', function ($scope) {
+  })
+  .controller('WorkCompletedCtrl', function ($scope, surveyWorkCompleted) {
+    $scope.rows = [];
+    $scope.loading = true;
+    $scope.error = false;
+
+    surveyWorkCompleted.byTeam()
+      .then(function (response) {
+        $scope.rows = response.rows;
+      })
+      .catch(function (error) {
+        $scope.error = true;
+        console.log(error);
+      })
+      .finally(function () {
+        $scope.loading = false;
+      });
   });
