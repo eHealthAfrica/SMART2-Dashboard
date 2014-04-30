37ef3cebaa8f78bc6b50b917e1232cb323a81a6d
Haykel Ben Jemia <hbj@allmas.tn>
2014-04-30 19:28:22 +0100

    Add charts for ‘Distribution of Age’. Refs #15.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -19,4 +19,43 @@ angular.module('smart2App')
       .finally(function () {
         $scope.loading = false;
       });
+  })
+  .controller('DistributionOfAgeCtrl', function ($scope, distributionOfAge) {
+    $scope.children = [];
+    $scope.members = [];
+    $scope.loading = true;
+    $scope.error = false;
+
+    distributionOfAge.byTeam()
+      .then(function (response) {
+        var children = [];
+        var members = [];
+
+        response.rows.forEach(function (row) {
+          var target = (row.key[0] === 'children') ? children : members;
+          target[row.key[1]] = [row.key[1], row.value];
+        });
+
+        [children, members].forEach(function (data) {
+          for (var i = 0; i < data.length; i++) {
+            if (data[i] === undefined)
+              data[i] = [i, 0];
+          }
+        });
+
+        $scope.children = [
+          { values: children }
+        ];
+
+        $scope.members = [
+          { values: members }
+        ];
+      })
+      .catch(function (error) {
+        $scope.error = true;
+        console.log(error);
+      })
+      .finally(function () {
+        $scope.loading = false;
+      });
   });
