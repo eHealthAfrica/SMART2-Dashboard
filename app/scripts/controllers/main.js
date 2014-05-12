b3d17568f62d5ae171026e18011b1e3e1783bde3
Haykel Ben Jemia <hbj@allmas.tn>
2014-05-12 16:58:43 +0100

    Implement 'Most recent submissions' view.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,6 +3,23 @@
 angular.module('smart2App')
   .controller('MainCtrl', function ($scope) {
   })
+  .controller('MostRecentSubmissionsCtrl', function ($scope, mostRecentSubmissions) {
+    $scope.rows = [];
+    $scope.loading = true;
+    $scope.error = false;
+
+    mostRecentSubmissions.byTeam()
+      .then(function (response) {
+        $scope.rows = response.rows;
+      })
+      .catch(function (error) {
+        $scope.error = true;
+        console.log(error);
+      })
+      .finally(function () {
+        $scope.loading = false;
+      });
+  })
   .controller('WorkCompletedCtrl', function ($scope, surveyWorkCompleted) {
     $scope.rows = [];
     $scope.loading = true;
