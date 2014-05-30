2c7f6e62952b0368f9f81bbe47cf116a053128ba
Haykel Ben Jemia <hbj@allmas.tn>
2014-05-30 17:54:05 +0100

    Display survey inputs plotted on a map. Fixes #32.

--- test/spec/design_docs/survey_work_completed.js
+++ test/spec/design_docs/survey_work_completed.js
@@ -28,7 +28,37 @@ describe('Work Completed View', function () {
   });
 
   var results = [
-    {clusters: 2, measured: {women: 4, children: 4}, hh: {n: 4, min: 1, max: 3, mean: 2, members: {n: 13, min: 2, max: 4, mean: 3.25}, women: {n: 4, min: 1, max: 1, mean: 1}, children: {n: 4, min: 0, max: 2, mean: 1}}},
-    {clusters: 1, measured: {women: 1, children: 6}, hh: {n: 3, min: 3, max: 3, mean: 3, members: {n: 12, min: 0, max: 10, mean: 4}, women: {n: 2, min: 0, max: 2, mean: 0.67}, children: {n: 7, min: 0, max: 5, mean: 2.33}}}
+    {
+      clusters: 2,
+      measured: {women: 4, children: 4},
+      hh: {
+        n: 4,
+        min: 1,
+        max: 3,
+        mean: 2,
+        members: {n: 13, min: 2, max: 4, mean: 3.25},
+        women: {n: 4, min: 1, max: 1, mean: 1},
+        children: {n: 4, min: 0, max: 2, mean: 1},
+        gpslocs: [
+          {lon: -111.86770775, lat: 40.741767116666665},
+          {lon: -111.86770775, lat: 40.741767116666665},
+          {lon: -111.86770775, lat: 40.741767116666665}
+        ]
+      }
+    },
+    {
+      clusters: 1,
+      measured: {women: 1, children: 6},
+      hh: {
+        n: 3,
+        min: 3,
+        max: 3,
+        mean: 3,
+        members: {n: 12, min: 0, max: 10, mean: 4},
+        women: {n: 2, min: 0, max: 2, mean: 0.67},
+        children: {n: 7, min: 0, max: 5, mean: 2.33},
+        gpslocs: []
+      }
+    }
   ];
 });
